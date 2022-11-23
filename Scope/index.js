91f270ef88efdaf44e3e5ef357aa0727e6b049da
Abhaya <shankarabhaya9@gmail.com>
2022-11-23 20:24:03 +0530

    first commit

---
+++ Scope/index.js
@@ -0,0 +1,22 @@
+function a() {
+  // d();
+  // function d() {
+  //   let b = 2;
+  //   console.log(b);
+  // }
+}
+/*1  */
+a();
+let b = 7;
+console.log(018 - 015);
+/*2  
+var b = 10;   */
+
+/*
+Answer : 
+
+a. Undefined        b. Not Defined
+c. 7                d. 10
+
+*/
+// ABHAYA SHANKAR
