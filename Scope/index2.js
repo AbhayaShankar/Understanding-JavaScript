7e241971026f030dde7ad28b118d5906e3e18e12
Abhaya <shankarabhaya9@gmail.com>
2022-11-23 21:35:15 +0530

    errors

--- Scope/index2.js
+++ Scope/index2.js
@@ -1,9 +1,27 @@
 let a = 10;
 
+var b = 25;
 console.log(b);
 
-var b = 25;
+var b = 250;
 
 console.log(a);
+
+console.log(b);
+
+// let a = 100; //redeclaration of let variable
+// will give a SYNTAX_ERROR.
+
+//ABHAYA SHANKAR
+
+let d;
+
+d = 20;
+
+console.log(d);
+
+const e = 200;
+e = 100; // Here redeclaring a const variable gives a typeError
+console.log(e);
 
 //ABHAYA SHANKAR
