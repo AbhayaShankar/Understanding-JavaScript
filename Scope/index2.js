f70cc8acd98560bf56ee6e9f3476299f99e09bc7
Abhaya <shankarabhaya9@gmail.com>
2022-12-01 19:15:05 +0530

    try...catch

--- Scope/index2.js
+++ Scope/index2.js
@@ -1,29 +1,75 @@
-console.log(b1);
-console.log(a1);
+// console.log(b1);
+// console.log(a1);
 
-let a1 = 5;
+// let a1 = 5;
 
-console.log(a1);
+// console.log(a1);
 
-var b1 = 15;
+// var b1 = 15;
 
-console.log(b1);
+// console.log(b1);
+
+// //ABHAYA SHANKAR
+
+// // let a = 100; //redeclaration of let variable
+// // will give a SYNTAX_ERROR.
+
+// //ABHAYA SHANKAR
+
+// let d1;
+
+// d1 = 20;
+
+// console.log(d1);
+
+// const e1 = 200;
+// e1 = 100; // Here redeclaring a const variable gives a typeError
+// console.log(e1);
 
 //ABHAYA SHANKAR
 
-// let a = 100; //redeclaration of let variable
-// will give a SYNTAX_ERROR.
+//Code script will die before error is handled by try...catch;
+/*
+try {
+  setTimeout(function () {
+    noSuchVariable;
+  }, 1000);
+} catch (err) {
+  alert("won't work");
+}
+*/
 
-//ABHAYA SHANKAR
+//Script should die here but it is not dying here... What could be the reason ????
 
-let d1;
+// Handling using try...catch & Settimeout function.
 
-d1 = 20;
+/*
 
-console.log(d1);
+setTimeout(function () {
+  try {
+    noSuchVariable;
+  } catch (err) {
+    alert("won't work");
+  }
+}, 1000);
 
-const e1 = 200;
-e1 = 100; // Here redeclaring a const variable gives a typeError
-console.log(e1);
+*/
+/*
+try {
+  errorHappens;
+} catch (error) {
+  alert(error.name);
+  alert(error.message);
+  alert(error.stack);
+}
 
-//ABHAYA SHANKAR
+*/
+
+let json = '{"data" : "Abhaya" , "age" : 30 }';
+
+try {
+  let user = JSON.parse(json);
+  alert(user.sex); //we are accessing something which is not defined in the json so different error rather than going to catch.
+} catch (err) {
+  alert("Something is wrong.");
+}
