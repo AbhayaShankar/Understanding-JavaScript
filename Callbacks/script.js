72e7ce1956f7d5ce729332c76d6bbe7e82d3db95
Abhaya <shankarabhaya9@gmail.com>
2022-12-04 00:02:01 +0530

    first

---
+++ Callbacks/script.js
@@ -0,0 +1,33 @@
+/*
+function loadScript(src) {
+  let script = document.createElement("script");
+  script.src = src;
+  document.head.append(script);
+}
+
+function loadScript(src, callback) {
+  let script = document.createElement("script");
+  script.src = src;
+
+  script.onload = () => {
+    callback(script);
+  };
+
+  document.head.append(script);
+}
+
+*/
+function loadScript(src, callback) {
+  let script = document.createElement("script");
+  script.src = src;
+  script.onload = () => callback(script);
+  document.head.append(script);
+}
+
+loadScript(
+  "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js",
+  (script) => {
+    alert(`Cool, the script ${script.src} is loaded`);
+    alert(_); // _ is a function declared in the loaded script
+  }
+);
